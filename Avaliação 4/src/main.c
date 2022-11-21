243a55d582ea61a7919705170888629d99087b75
Thiago Mazzoni <56179724+MazzoniXD@users.noreply.github.com>
2022-11-21 19:22:27 -0300

    Add files via upload

--- Avaliação 4/src/main.c
+++ Avaliação 4/src/main.c
@@ -2,12 +2,12 @@
 
 int main(){
 	// Variaveis para o programa
-	int num_falta, n1, n2, n3;
+	int num_falta, n1, n2, n3, tamanho;
 
 	n1 = 3;
 	n2 = 2;
 	n3 = 9;
-	
+
 	// Exemplo 1
 	int v[n1];
 	v[0] = 3;	v[1] = 0;	v[2] = 1;
@@ -21,20 +21,35 @@ int main(){
 	m[0] = 9;	m[1] = 6;	m[2] = 4;	m[3] = 2;	m[4] = 3;
 	m[5] = 5;	m[6] = 7;	m[7] = 0;	m[8] = 1;
 
+	// Teste para qualquer tamanho
+	printf("\nDigite o tamanho do vetor que voce quer testar: ");
+	scanf("%d",&tamanho);
+
+	// Criar o vetor pelo tamanho escolhido
+	int vetor[tamanho];
+
+	// Digitar os valores que vão ficar no Vetor
+	for(int i = 0; i < tamanho; i++){
+		printf("\nDigite o valor na posicao %d: ", i+1);
+		scanf("%d", &vetor[i]);
+	}
+
 	//Fazendo a chamada de cada exemplo em uma função
 	num_falta = verificar(v, n1);
-	printf("Numero que falta no primeiro exemplo: %d\n", num_falta);
+	printf("\nNumero que falta no primeiro exemplo: %d\n", num_falta);
 
 	num_falta = verificar(t, n2);
-	printf("Numero que falta no segundo exemplo: %d\n", num_falta);
+	printf("\nNumero que falta no segundo exemplo: %d\n", num_falta);
 
 	num_falta = verificar(m, n3);
-	printf("Numero que falta no ultimo exemplo: %d\n", num_falta);
+	printf("\nNumero que falta no ultimo exemplo: %d\n", num_falta);
 
+	num_falta = verificar(vetor, tamanho);
+	printf("\nNumero que falta no tanhanho aleatorio: %d\n", num_falta);
 	return (0);
 }
 
-	//Função para indentificar qual número que falta
+//Função para indentificar qual número que falta
 int verificar(int vetor[], int tamanho){
 	int i, somador, testador;
 	testador = 0;
